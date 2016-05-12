7b8375d2b54b807d2fd799877887ff3497bc4408
Rob <robert.maglieri@gmail.com>
2016-05-12 16:50:15 -0400

    add information controller

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 var app = angular.module('myApp', []);
+
 app.controller('pets', function($scope, $http) {
   $http.get("http://localhost/PMR/php/getPatientData.php").then(function (response) {
     console.log(response)
     $scope.names = response.data.things;
   });
 });
+
+app.controller('infoCtrl', function($scope){
+	$scope.myTable = true;
+	$scope.myCarousel = false;
+    $scope.toggle = function() {
+    	$scope.myTable = !$scope.myTable;
+        $scope.myCarousel = !$scope.myCarousel;
+    }
+});
+
