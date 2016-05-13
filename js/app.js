3f0e72101fb4372ae0a9689752cbb04f237f78d1
Rob <robert.maglieri@gmail.com>
2016-05-13 14:41:45 -0400

    Test From Visual Studio Code

--- js/app.js
+++ js/app.js
@@ -1,18 +1,48 @@
 var app = angular.module('myApp', []);
 
+//Populated the table
 app.controller('pets', function($scope, $http) {
   $http.get("http://localhost/PMR/php/getPatientData.php").then(function (response) {
-    console.log(response)
     $scope.names = response.data.things;
   });
 });
 
+// Controller to toggle the view wrt collapsible button selection
+//TODO Make it modular!
 app.controller('infoCtrl', function($scope){
 	$scope.myTable = true;
 	$scope.myCarousel = false;
-    $scope.toggle = function() {
-    	$scope.myTable = !$scope.myTable;
-        $scope.myCarousel = !$scope.myCarousel;
-    }
+  $scope.toggle = function() {
+   $scope.myTable = !$scope.myTable;
+   $scope.myCarousel = !$scope.myCarousel;
+ }
 });
 
+// Controller to get patient list and store in scope
+/*app.controller('patientListCtrl', function($scope, $http) {
+    $http.get("welcome.htm")
+    .then(function(response) {
+        $scope.PatientList = response.data;
+    });
+*/
+
+// Controller to request the ptient's data from the server
+/*app.controller('patientDataCtrl', function($scope, $http) {
+    $http.get("welcome.htm")
+    .then(function(response) {
+        $scope.PatientToDisplay = response.data;
+    });
+*/
+
+// Make patient class to store data
+function patient(patient_id){
+  //Generic patient info, name, age, doctor, diagnosis
+  $scope.PatientToDisplay.info = function(){
+
+  };
+  // List of patient docs
+  $scope.PatientToDisplay.docs = function(){
+
+  };
+  
+}
