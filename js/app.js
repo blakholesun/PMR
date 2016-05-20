3d073c85e3b6c988aaf2baad2f79b35841181563
Rob <robert.maglieri@gmail.com>
2016-05-20 10:13:26 -0400

    Get patients sorted by doctor and populate the menu

--- js/app.js
+++ js/app.js
@@ -5,17 +5,30 @@ app.controller('pageController', function($http,$scope,$timeout){
   // Grab the list of patients and categorize by doctor
   $http.get("http://localhost/PMR/php/getPatientData_local.php").then(function (response) {
     $scope.data=response.data.things;
-
-    var dataList = [];
+    function Doctor(){
+      this.firstName = "";
+      this.patients = [];
+    }
+    var dataList = []; // the list of doctor objects with patients
+    var docs = []; // create an array to hold doctor names
     var index;
     for (var i = 0; i < $scope.data.length; i++){
-      index = dataList.indexOf($scope.data[i].owner);
+      index = docs.indexOf($scope.data[i].owner);
       if (index === -1){
-        dataList.push($scope.data[i].owner);
+        docs.push($scope.data[i].owner);
+        dataList.push(new Doctor());
+        dataList[dataList.length-1].name = $scope.data[i].owner;
+        console.log($scope.data[i].owner);
+        for (var j = 0; j<$scope.data.length; j++){
+          if (docs[docs.length-1] === $scope.data[j].owner){
+            dataList[dataList.length-1].patients.push($scope.data[j].name)
+          }
+        }
       }
-      index = dataList.indexOf($scope.data[i].owner);
-      console.log(dataList);
     }
+    $scope.dataList = dataList;
+    $scope.activePatient = 0; // First patient for first doctor
+    console.log($scope.dataList);
   });
 
   // Grab the data for each patient
